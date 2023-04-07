35d5f605a44dda7bb42b60d6201cc3c72fa8a5f4
feegomez <federico.gomez@everis.nttdata.com>
2023-04-07 13:47:38 -0300

    Update checksLogin.js

--- src/middleware/checksLogin.js
+++ src/middleware/checksLogin.js
@@ -1,11 +1,11 @@
 const {check} = require("express-validator")
 const checksLogin  = [
     check("email")
-        .not().isEmpty().withMessage("el campo email es obligatorio")
+        .notEmpty().withMessage("el campo email es obligatorio")
         .isString().withMessage("el campo tipo debe ser de email string")
         .isEmail().withMessage("el string ingresado debe ser un email"),
     check("password")
-        .not().isEmpty().withMessage("el campo password es obligatorio")
+        .notEmpty().withMessage("el campo password es obligatorio")
         .isString().withMessage("el campo password debe ser de tipo string"),
 ]
 module.exports = checksLogin
